f1c140b56cc57e77d4a1a67163244d5ec877e8ec
Pedro-de-Lima <luizp1163@gmail.com>
2022-07-20 13:02:54 -0300

    finalizado a aba de login com o botao ja com funcionalidade

--- app/src/main/java/com/example/appinovia/FormLogin.kt
+++ app/src/main/java/com/example/appinovia/FormLogin.kt
@@ -1,7 +1,9 @@
 package com.example.appinovia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class FormLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +11,17 @@ class FormLogin : AppCompatActivity() {
         setContentView(R.layout.activity_form_login)
 
         supportActionBar!!.hide()
+
+        val botaoLogin = findViewById<Button>(R.id.botaoLogin)
+
+        botaoLogin.setOnClickListener{
+            irParaTelaPrincipal()
+        }
+
+    }
+
+    private fun irParaTelaPrincipal() {
+        val segundaTela = Intent(this, MainActivity::class.java)
+        startActivity(segundaTela)
     }
 }
