91159c013298a362d33eacf8df11d0d3f01ee201
Pedro-de-Lima <luizp1163@gmail.com>
2022-07-26 12:55:23 -0300

    finalizado parte visual da tela de seleção da parte do corpo

--- app/src/main/java/com/example/appinovia/TelaPreSessao.kt
+++ app/src/main/java/com/example/appinovia/TelaPreSessao.kt
@@ -1,6 +1,9 @@
 package com.example.appinovia
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class TelaPreSessao : AppCompatActivity() {
@@ -10,5 +13,24 @@ class TelaPreSessao : AppCompatActivity() {
 
         supportActionBar!!.hide()
 
+        val botaoVoltar = findViewById<ImageButton>(R.id.botaoVoltar)
+        val viewAbdomenCosta = findViewById<View>(R.id.viewAbdomenCosta)
+
+        viewAbdomenCosta.setOnClickListener {
+            if(viewAbdomenCosta.isClickable)
+        }
+
+        botaoVoltar.setOnClickListener{
+            voltar()
+        }
+    }
+
+    private fun visivel(){
+
+    }
+
+    private fun voltar(){
+        val voltar = Intent(this, TelaPassoaPasso::class.java)
+        startActivity(voltar)
     }
 }
